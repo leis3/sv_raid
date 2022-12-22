76fab0eaaab87d942c76ea11945bb46127ddc7e5
leis <uthmar33@gmail.com>
2022-12-22 19:12:51 +0900

    `star`でフィルタする関数を追加

--- src/lib.rs
+++ src/lib.rs
@@ -7,7 +7,6 @@ pub use raid::Raid;
 pub use pokemon::{Type, Category, Ability, Move};
 
 use error::Error;
-use itertools::Itertools;
 
 /// テラレイドに出現するポケモンの情報を返す。
 /// ポケモンの名前`name`と難易度`star`を指定する。
@@ -26,15 +25,15 @@ pub fn raid(name: &str, star: u8) -> Result<Raid, Error> {
 
     let types = raid_info.types.iter().map(|s| {
         Type::try_from(s.as_str()).unwrap()
-    }).collect_vec();
+    }).collect();
 
     let moves = raid_info.moves.iter().map(|s| {
         mv.iter().find(|m| &m.name == s).unwrap().clone()
-    }).collect_vec();
+    }).collect();
 
     let abilities = raid_info.abilities.iter().map(|s| {
         abl.iter().find(|a| &a.name == s).unwrap().clone()
-    }).collect_vec();
+    }).collect();
 
     Ok(Raid {
         name: name.to_string(),
@@ -47,4 +46,37 @@ pub fn raid(name: &str, star: u8) -> Result<Raid, Error> {
         sp_def: raid_info.sp_def,
         speed: raid_info.speed,
     })
+}
+
+// \`star\`で指定した難易度のテラレイドのリストを返す。
+pub fn filter_by_star(star: u8) -> Vec<Raid> {
+    let mv = data::mv();
+    let abl = data::abl();
+    let raid = data::raid();
+
+    let Some(data) = raid.get(&star) else { return Vec::new(); };
+
+    data.iter().map(|r| {
+        let types = r.types.iter().map(|s| {
+            Type::try_from(s.as_str()).unwrap()
+        }).collect();
+        let abilities = r.abilities.iter().map(|s| {
+            abl.iter().find(|a| &a.name == s).unwrap().clone()
+        }).collect();
+        let moves = r.moves.iter().map(|s| {
+            mv.iter().find(|m| &m.name == s).unwrap().clone()
+        }).collect();
+
+        Raid {
+            name: r.name.clone(),
+            star, types, abilities, moves,
+            actions: r.actions.clone(),
+            hp: r.hp,
+            atk: r.atk,
+            def: r.def,
+            sp_atk: r.sp_atk,
+            sp_def: r.sp_def,
+            speed: r.speed,
+        }
+    }).collect()
 }
